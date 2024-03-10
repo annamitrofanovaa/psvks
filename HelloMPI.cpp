b44b1f53df5152de8d4415d7ef42dd2f06b00a0e
annamitrofanova <22amitrofa@gmail.com>
2024-03-10 21:57:24 +0300

    dz

--- HelloMPI.cpp
+++ HelloMPI.cpp
@@ -3,20 +3,20 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-    // ������������� MPI
+    // Инициализация MPI
     MPI_Init(&argc, &argv);
 
-    // ��������� ������ ����� ��������� � ����� �������� ��������
+    // Получение общего числа процессов и ранга текущего процесса
     int numProcesses, rank;
     
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
     
 
-    // ����� "Hello world" �� ������� ��������
+    // Вывод "Hello world" из каждого процесса
     std::cout << "Hello world from process " << rank << " of " << numProcesses << std::endl;
    // printf("Hello world from process = %d, total munder of processes: %d\n", rank, numProcesses);
-    // ����������� MPI
+    // Финализация MPI
     MPI_Finalize();
 
     return 0;
--- openMp.cpp
+++ openMp.cpp
@@ -2,17 +2,17 @@
 #include <omp.h>
 
 int main() {
-    // ���������, ��� ��������� ���� ���� ������ ����������� �����������
+    // Указываем, что следующий блок кода должен выполняться параллельно
 //#pragma omp parallel
 #pragma omp parallel num_threads(4)
     {
-        // �������� ����� ���������� �������
+        // Получаем общее количество потоков
         int num_threads = omp_get_num_threads();
 
-        // �������� ����� �������� ������
+        // Получаем номер текущего потока
         int thread_num = omp_get_thread_num();
 
-        // ������� ��������� � �������������� ������ ������
+        // Выводим сообщение с использованием номера потока
 #pragma omp critical
         {
             std::cout << "Heello world from thread " << thread_num + 1 << " of " << num_threads << std::endl;
